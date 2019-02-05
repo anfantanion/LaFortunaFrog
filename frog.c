f5e4786992299ba1243e36ba4ff0dc6826ed57fa
Philip <32490263+anfantanion@users.noreply.github.com>
2019-02-05 16:14:00 +0000

    Imporvements

--- frog.c
+++ frog.c
@@ -21,10 +21,19 @@
 #define frogSize 12
 #define frogColour LIME_GREEN
 
+void drawVehicles();
+void drawFrog();
+void drawRoads();
+void updateVehicles();
+void collision();
+void collideHandler();
+void drawStats();
+
 const uint16_t colours[numTracks] = {DARK_BLUE,FOREST_GREEN,DARK_BLUE,DARK_BLUE,DARK_BLUE,DARK_BLUE,DARK_BLUE,DARK_VIOLET,BLACK,BLACK,BLACK,BLACK,BLACK,DARK_VIOLET};
 volatile uint8_t lives = 3;
 volatile uint16_t score = 0;
-volatile uint16_t fps = 0;
+volatile uint8_t fps = 0;
+volatile uint8_t collide = 1;
 
 typedef struct {
 	uint8_t prevPos;
@@ -38,8 +47,8 @@ typedef struct {
 	uint8_t prevX;
 } frog;
 
-const int8_t roadLaneSpeed[numRoadLanes] = {1,3,1,2,1};
-const uint8_t vehicleLength[numRoadLanes] = {2*unitLength,1*unitLength,1*unitLength, 1*unitLength,1*unitLength};
+const int8_t roadLaneSpeed[numRoadLanes] = {-1,3,1,2,1};
+const uint8_t vehicleLength[numRoadLanes] = {2*unitLength,2*unitLength,1*unitLength, 1*unitLength,1*unitLength};
 const uint16_t laneColours[numRoadLanes] = {GOLD,GREEN_YELLOW,PALE_TURQUOISE,CRIMSON,LAVENDER};
 
 const frog defaultFrog = {14,13,120+frogSize/2,120+frogSize/2};
@@ -57,65 +66,58 @@ volatile frog mainFrog;
 volatile int8_t collided =0;
 
 
-void drawVehicles();
-void drawFrog();
-void drawRoads();
-void updateVehicles();
-void collision();
-void collideHandler();
-
-
+#define laneTopOffset 5
+#define laneBottomOffest 12
 
 void drawVehicles(){
 	//Draw road vehicles
 	for(uint8_t lane = 0; lane<numRoadLanes;lane++){
-		int16_t currentlane = (roadOffset + lane) * trackWidth -5;
-		int8_t size = vehicleLength[lane];
+		uint16_t currentlane = (roadOffset + lane -1) * trackWidth + offsetPx + laneTopOffset;
+		uint8_t size = vehicleLength[lane];
 		int8_t vSpeed = roadLaneSpeed[lane];
 
 		for (uint8_t ve = 0; ve<maxVehiclesPerLane;ve++){
 			vehicle vex = roadLanes[lane][ve];
 			//Moving Right
 			if (vSpeed > 0 ){
-				if (vex.pos < vex.prevPos && vex.prevPos-vex.pos >200 ){
-					rectangle newR = {0,vex.pos,currentlane,currentlane+15};
-					rectangle oldR = {vex.prevPos-size,screenWidth,currentlane,currentlane+15};
+				//Moved off right side of screen
+				if (vex.pos < vex.prevPos){
+					rectangle newR = {0,vex.pos,currentlane,currentlane + laneBottomOffest};
+					rectangle oldR = {vex.prevPos-size,screenWidth,currentlane,currentlane + laneBottomOffest};
 					fill_rectangle(newR,laneColours[lane]);
 					fill_rectangle(oldR,BLACK);
 					roadLanes[lane][ve].prevPos = vex.pos;
 				}
 				//Moving right
 				else if (vex.pos > vex.prevPos){
-					rectangle newR = {vex.prevPos,vex.pos,currentlane,currentlane+15};
-					rectangle oldR = {vex.prevPos-size,vex.pos-size,currentlane,currentlane+15};
+					rectangle newR = {vex.prevPos,vex.pos,currentlane,currentlane + laneBottomOffest};
+					rectangle oldR = {vex.prevPos-size,vex.pos-size,currentlane,currentlane + laneBottomOffest};
 					fill_rectangle(newR,laneColours[lane]);
 					fill_rectangle(oldR,BLACK);
 					roadLanes[lane][ve].prevPos = vex.pos;
 				}
 			}
 			else{ //Moving Left
-				if (vex.pos > vex.prevPos && vex.pos-vex.prevPos >200 ){
-					rectangle newR = {vex.pos-size,screenWidth,currentlane,currentlane+15};
-					rectangle oldR = {0,vex.prevPos-size,currentlane,currentlane+15};
+				//Moving of left side of screen
+				uint8_t leftPos = vex.pos-size;
+				uint8_t leftPrevPos = vex.prevPos-size;
+				if (leftPos > leftPrevPos){
+					rectangle newR = {leftPos,screenWidth,currentlane,currentlane + laneBottomOffest};
+					rectangle oldR = {0,leftPrevPos,currentlane,currentlane + laneBottomOffest};
 					fill_rectangle(newR,HOT_PINK);
-					fill_rectangle(oldR,BLACK);
+					fill_rectangle(oldR,GREY);
 					roadLanes[lane][ve].prevPos = vex.pos;
 				}
-				else if (vex.pos < vex.prevPos){
-					rectangle newR = {vex.pos-size,vex.prevPos-size,currentlane,currentlane+15};
-					rectangle oldR = {vex.pos,vex.prevPos,currentlane,currentlane+15};
-					if(vex.pos<size){
-						newR = (rectangle){0,0,currentlane,currentlane+15};
-						oldR = (rectangle){vex.pos,vex.prevPos,currentlane,currentlane+15};
-					}
-					else{
-						newR = (rectangle){vex.pos-size,vex.prevPos-size,currentlane,currentlane+15};
-						oldR = (rectangle){vex.pos,vex.prevPos,currentlane,currentlane+15};
-					}
+				//Moving right
+				if (leftPos < leftPrevPos){
+					rectangle newR = {vex.pos-size, vex.prevPos-size,currentlane,currentlane + laneBottomOffest};
+					rectangle oldR = {vex.pos,vex.prevPos,currentlane,currentlane + laneBottomOffest};
 					fill_rectangle(newR,laneColours[lane]);
 					fill_rectangle(oldR,BLACK);
 					roadLanes[lane][ve].prevPos = vex.pos;
 				}
+
+				
 			}
 			
 			 
@@ -123,6 +125,7 @@ void drawVehicles(){
 
 	}
 }
+
 
 void drawFrog(){
 	if(mainFrog.prevTrack!=mainFrog.track || mainFrog.prevX!=mainFrog.x){
@@ -157,26 +160,46 @@ void updateVehicles(){
 }
 
 void collision(){
-	if (roadOffset<=mainFrog.track && mainFrog.track<numTracks )
-	for (uint8_t ve = 0; ve<maxVehiclesPerLane;ve++){
-			vehicle vex = roadLanes[mainFrog.track-roadOffset][ve];
-			if (
-				mainFrog.x < vex.pos  &&  mainFrog.x-frogSize > vex.pos-vehicleLength[mainFrog.track-roadOffset]
-				||
-				mainFrog.x-frogSize < vex.pos  &&  mainFrog.x-frogSize > vex.pos-vehicleLength[mainFrog.track-roadOffset]
-				||
-				mainFrog.x > vex.pos-vehicleLength[mainFrog.track-roadOffset]  &&  mainFrog.x < (vex.pos)
-			){
-				collideHandler();
-			}
+	if (collide && roadOffset<=mainFrog.track && mainFrog.track<numTracks){ //If on road
+		for (uint8_t ve = 0; ve<maxVehiclesPerLane;ve++){
+				vehicle vex = roadLanes[mainFrog.track-roadOffset][ve];
+				if (
+					(mainFrog.x < vex.pos  &&  mainFrog.x-frogSize > vex.pos-vehicleLength[mainFrog.track-roadOffset])
+					||
+					(mainFrog.x-frogSize < vex.pos  &&  mainFrog.x-frogSize > vex.pos-vehicleLength[mainFrog.track-roadOffset])
+					||
+					(mainFrog.x > vex.pos-vehicleLength[mainFrog.track-roadOffset]  &&  mainFrog.x < (vex.pos))
+				){
+					collideHandler();
+				}
+		}
 	}
 }
 
 void collideHandler(){
+	collide = 0;
 	lives-=1;
-	mainFrog = defaultFrog;
+	mainFrog.x = defaultFrog.x;
+	mainFrog.track = defaultFrog.track;
 	drawFrog();
 	drawVehicles();
+	collide = 1;
+}
+
+void drawStats(){
+	char buffer[4];
+	sprintf(buffer, "%03d", fps);
+	display_string_xy(buffer, 0, 0);
+
+	sprintf(buffer, "%03d", lives);
+	display_string_xy("Lives:", 20, 0);
+	display_string_xy(buffer, 60, 0);
+
+	sprintf(buffer, "%03d", score);
+	display_string_xy("Score:", 80, 0);
+	display_string_xy(buffer, 120, 0);
+	fps = 0;
+
 }
 
 ISR(INT6_vect)
@@ -190,7 +213,6 @@ ISR(INT6_vect)
 //Movement
 ISR(TIMER1_COMPA_vect){	
 	if (center_pressed()){
-		
 	}
 	if(left_pressed() && mainFrog.x>22){
 		mainFrog.x-=22;
@@ -206,26 +228,12 @@ ISR(TIMER1_COMPA_vect){
 	}
 	updateVehicles();
 	collision();
+	drawStats();
 }
 
 ISR(TIMER3_COMPA_vect)
 {
-	char buffer[4];
-	sprintf(buffer, "%03d", fps);
-	display_string_xy(buffer, 0, 0);
-	fps = 0;
-
-	char buffer2[4];
-	sprintf(buffer2, "%03d", lives);
-	display_string_xy("Lives:", 20, 0);
-	display_string_xy(buffer2, 60, 0);
-	fps = 0;
-
-	char buffer3[4];
-	sprintf(buffer3, "%03d", score);
-	display_string_xy("Score:", 80, 0);
-	display_string_xy(buffer3, 120, 0);
-	fps = 0;
+	drawStats();
 }
 
 void main() {
@@ -251,16 +259,20 @@ void main() {
 	/* Enable game timer interrupt (Timer 1 CTC Mode 4) */
 	TCCR1A = 0;
 	TCCR1B = _BV(WGM12);
-	TCCR1B |= _BV(1);  //Timer Scale
+	//TCCR1B |= _BV(1);  //Timer Scale
+	//OCR1A = 31250; //Timer trigger value
+
 	TIMSK1 |= _BV(OCIE1A);
-	OCR1A = 31250; //Timer trigger value
+
+	TCCR1B |= _BV(2);
+	OCR1A = 10000;
 
 	while (1){
 		collided = 0;
 		mainFrog = defaultFrog;
 		drawRoads();
 		sei();
-		while (lives!=0){}
+		while (1){}
 		cli();
 		drawFrog();
 		display_string_xy("GAME OVER", 100, 50);
